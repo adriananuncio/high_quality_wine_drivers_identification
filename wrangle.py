ae978c15979ee6c4ce9d1d6db389605512a9310d
Mack <122935207+mack-mcglenn@users.noreply.github.com>
2023-04-18 10:10:26 -0500

    Update wrangle.py
    updated with acq, prep, scale, and cluster functions

--- wrangle.py
+++ wrangle.py
@@ -5,45 +5,100 @@
 
 # In[ ]:
 
+############ Acquire #####################
+def acquire_wine(df):
+    """ This function will access and concat the red wine and white wine csvs acquired from the data.world dataframes
+    for preparation"""
+    if os.path.isfile('wine.csv'):
+        return pd.read_csv('wine.csv')
+    else: 
+        # Calling in my dfs from csv link
+        red = pd.read_csv('https://query.data.world/s/azffrkwaoqlfrd3srbnuwjp24hvlj4?dws=00000')
+        white = df = pd.read_csv('https://query.data.world/s/6ao5pdvepveo2qeeafwdfia6bl5mou?dws=00000')
+        
+        # Adding 'type' categories on both dataframes before concating
+        red['type'] = 'red'
+        white['type'] = 'white'
+        
+        # The two become one
+        df = wine = pd.concat([red, white], index=False)
+        return df
+      
+  
+############ Prepare #####################
+def prep_wine(df):
+    """ This function will concat the red wine and white wine csvs acquired from the data.world dataframes
+    and prepare them for exploration and analysis"""
+    # Creating variable to categorize quality
+    df['quality_bins'] = pd.cut(df.quality,[0,5,7,9], labels=['low_quality', 'mid_quality', 'high_quality'])
+    # Creating dummy variables for type
+    dummies = pd.get_dummies(data=df[['type']], dummy_na= False, drop_first=False)
+    df = pd.concat([df, dummies], axis = 1)
+    return df
+  
+############ Split #####################
+def split_wine(df, target):
+    '''
+    take in a DataFrame return train, validate, test split on wine DataFrame.
+    '''
+# Reminder: I don't need to stratify in regression. I don't remember why, but Madeleine said 
+# it
+     train, test = train_test_split(df, test_size=.2, random_state=123, stratify=df[target])
+    train, val = train_test_split(train, test_size=.30, random_state=123, stratify=train[target])
+    return train, val, test  
 
-# after the free sulfur dioxide outlier has been removed
-plt.figure(figsize=[15,15])
-plt.subplot(421)
-plt.title('Free Sulfur Dioxide Distibution Values \nw/o High FSD Val')
-train.free_sulfur_dioxide.hist(color='lightsalmon', alpha = 0.7)
-plt.subplot(421)
-train[train['type']== 'white'].free_sulfur_dioxide.hist(color='rosybrown', alpha = 0.7)
-plt.subplot(421)
-train[train['type']== 'red'].free_sulfur_dioxide.hist(color='maroon', alpha = 0.9)
-plt.subplot(422)
-plt.title('pH Distribution Values \nw/o High FSD Val')
-train.pH.hist(color='lightsalmon', alpha = 0.7)
-plt.subplot(422)
-train[train['type']== 'white'].pH.hist(color='rosybrown', alpha = 0.7)
-plt.subplot(422)
-train[train['type']== 'red'].pH.hist(color='maroon', alpha = 0.9)
-plt.subplot(423)
-plt.title('Free Sulfer Dioxide x Wine Quality Distribution Values \nw/o High FSD Val')
-sns.boxplot(data=train, x='quality_bins', y='free_sulfur_dioxide', palette='rocket')
-plt.subplot(424)
-plt.title('pH x Wine Quality Distributions Values \nw/o High FSD Val')
-sns.boxplot(data=train, x='quality_bins', y='pH', palette='rocket')
-plt.subplot(425)
-plt.title('Free Sulfer Dioxide x pH \nw/o High FSD Val')
-sns.scatterplot(data=train, x='pH', y='free_sulfur_dioxide', hue='quality_bins', palette='rocket')
-plt.subplot(426)
-plt.title('Free Sulfur Dioxide x pH \nw/o High FSD Val')
-sns.scatterplot(data=train, x='pH', y='free_sulfur_dioxide', hue='type', palette='rocket')
-plt.subplot(427)
-plt.title('Free Sulfer Dioxide x pH \nw/o High FSD Val')
-sns.scatterplot(data=train, x='pH', y='free_sulfur_dioxide', color='chocolate')
-plt.tight_layout()
-plt.show()
+ ########### Scaling ################## 
+ def split_scaled(train, val, test):
+    """ This function returns scaled versions of certain features in train/val/test using a MinMax scaler """
+    # Features I want to scale
+    to_scale= ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar',
+       'chlorides', 'free sulfur dioxide', 'total sulfur dioxide', 'density',
+       'pH', 'sulphates', 'alcohol']
+    # Features I want to keep unscaled
+    not_to_scale = ['quality', 'type_red',
+       'type_white']
+    
+    # Making copies to scale
+    train_sc = train.copy()
+    val_sc = val.copy()
+    test_sc =test.copy()
 
-# min and max values for free sulfur dioxide and pH
-print(f'Free Sulfur Dioxide Min Value: {train.free_sulfur_dioxide.min()}')
-print(f'Free Sulfur Dioxide Max Value: {train.free_sulfur_dioxide.max()}')
-print('-'*50)
-print(f'pH Min Value: {train.pH.min()}')
-print(f'pH Max Value: {train.pH.max()}')
+    # Make the scaler
+    minmax= MinMaxScaler()
 
+    # Fit the scaler
+    minmax.fit(train[to_scale])
+    
+    # Make dataframes applying my minmax scaler & setting index
+    train_sc = pd.DataFrame(minmax.transform(train[to_scale]), columns = train[to_scale].columns.values).set_index([train.index.values])
+    val_sc = pd.DataFrame(minmax.transform(val[to_scale]), columns = val[to_scale].columns.values).set_index([val.index.values])
+    test_sc = pd.DataFrame(minmax.transform(test[to_scale]), columns = test[to_scale].columns.values).set_index([test.index.values])
+    
+    # Concating my scaled sets with my non_scaled
+    train_sc = pd.concat([train_sc, train[not_to_scale]], axis=1)
+    val_sc = pd.concat([val_sc, val[not_to_scale]], axis=1)
+    test_sc = pd.concat([test_sc, test[not_to_scale]], axis=1)
+
+    # (Notes to self:
+    # Make sure to put dataframes into brackets before tryng to concat!!!
+    # And indicate that you want it on one axis!!!!)
+    
+    return train_sc, val_sc, test_sc 
+  
+############# Clustering ################
+def clusters_sc(df, v1, v2):
+  """ This function takes in a dataframe and two independent variables from that dataframe and applies Kmeans
+  with 3 clusters"""
+    # Creating df with my 2 independent variables that I want to cluster. No need to scale since I'm using train_sc
+    df = train_sc[[v1, v2]]
+    
+    # Making the thing
+    kmc = KMeans(n_clusters = 3, random_state= 123)
+    # Fitting the thing
+    kmeans.fit(df)
+    # Predicting 
+    kmeans.predict(df)
+    
+    df['cluster_sc'] = kmeans.predict(df)
+    
+    return df
